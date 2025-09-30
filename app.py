0f2f0a2b103e5ce37a6f9bf00a0f159306362a9e
keqae <110154600+keqae@users.noreply.github.com>
2025-09-30 08:36:15 +0000

    Website background decoration

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # load the html page
 @app.route('/')
 def home():
-    return render_template('templates/index.html')
+    return render_template('index.html')
 
 # api endpoint
 @app.route('/solve', methods=['POST'])
